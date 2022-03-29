c97f371df2150a712290002f66a7c0a6203d2d6b
Manuel Thalmann <m@nuth.ch>
2022-03-29 22:51:24 +0200

    Refactor the testing environment

---
+++ test/runTests.ts
@@ -0,0 +1,9 @@
+import { runTests } from '@vscode/test-electron';
+import { resolve } from 'path';
+
+(async () => {
+    await runTests({
+        extensionDevelopmentPath: resolve(__dirname, '..', '..'),
+        extensionTestsPath: resolve(__dirname, 'index')
+    });
+})();
